ec386cfb4fa498203983454c8bb2be238538cd78
joseph <joseph@debian.(none)>
2010-08-16 10:12:33 +0100

    blah

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -63,7 +63,7 @@ class ProjectsController < ApplicationController
     @project.user = current_user
 
     @project.tasks.each do |t| # set every .nil task to current user
-	if t.user == nil
+	if t.user.nil?
 	   t.user = current_user
 	end
     end
