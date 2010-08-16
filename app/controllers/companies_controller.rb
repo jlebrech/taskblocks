dfcd30b04d3f7e91c0446dc63b0c4d1c7bf8c370
joseph <joseph@debian.(none)>
2010-08-16 15:27:53 +0100

    i

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -63,7 +63,7 @@ class CompaniesController < ApplicationController
 
     @proj = params
 
-    return render :json => @proj[:company][:projects_attributes]["0"] 
+    return render :json => @proj[:company][:projects_attributes]["0"][:user_id] 
 
     respond_to do |format|
       if @company.update_attributes(params[:company])
