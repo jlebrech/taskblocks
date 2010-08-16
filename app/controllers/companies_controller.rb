14281f97e7c85e4a69aa6a073ff70d890925ccd0
joseph <joseph@debian.(none)>
2010-08-16 13:59:20 +0100

    blah

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -61,7 +61,7 @@ class CompaniesController < ApplicationController
   def update
     @company = Company.find(params[:id])
 
-    return render :text => params[:company][:projects_attributes] 
+    return render :text => params[:company][:projects_attributes][0] 
 
     respond_to do |format|
       if @company.update_attributes(params[:company])
